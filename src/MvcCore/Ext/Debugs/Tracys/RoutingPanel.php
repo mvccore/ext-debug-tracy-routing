29548e82eed41e40bb976bbb5c26cc230d8a0e6b
Tom Flídr <tomflidr@gmail.com>
2024-03-14 14:51:54 +0100

    release v5.2.2

--- src/MvcCore/Ext/Debugs/Tracys/RoutingPanel.php
+++ src/MvcCore/Ext/Debugs/Tracys/RoutingPanel.php
@@ -24,7 +24,7 @@ class RoutingPanel implements \Tracy\IBarPanel {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.2.1';
+	const VERSION = '5.2.2';
 	
 
 	/**
